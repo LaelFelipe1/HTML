746a08a4b48a3fe076fa402211eaed8dd76daf25
Lael Felipe <157909023+LaelFelipe1@users.noreply.github.com>
2024-05-02 22:14:25 -0300

    Update MyProject.py

--- MyProject.py
+++ MyProject.py
@@ -16,6 +16,11 @@ azul = (0, 0, 255)
 largura, altura = 600, 400
 tela = pygame.display.set_mode((largura, altura))
 nome_jogo = pygame.display.set_caption("My Project")
+tela.fill(branco)
+
+#Criar traves
+rect1 = pygame.Rect(4, 170, 15, 60)
+rect2 = pygame.Rect(580, 170, 15, 60)
 
 
 # Movimentos
@@ -23,27 +28,31 @@ UP = (0, -20)
 DOWN = (0, 20)
 RIGHT = (20, 0)
 LEFT = (-20, 0)
-direcao_inicial = RIGHT
+
+
 # Loop
-deve_continuar = True
-while deve_continuar:
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                direcao_inicial = LEFT
+            if event.key == pygame.K_w:
+                rect1.move_ip(UP)
             elif event.key == pygame.K_s:
-                direcao_inicial = DOWN
-            elif event.key == pygame.K_d:
-                direcao_inicial = RIGHT
-            elif event.key == pygame.K_w:
-                direcao_inicial = UP
+                rect1.move_ip(DOWN)
+            elif event.key == pygame.K_DOWN:
+                rect2.move_ip(DOWN)
+            elif event.key == pygame.K_UP:
+                rect2.move_ip(UP)
+
     # Desenhar bola
-    pygame.draw.circle(tela, vermelho, (50, 50), 50, 50)
-    # Desenhar linha
-    pygame.draw.rect(tela, azul, (50, 300, 500, 50))
+    pygame.draw.circle(tela, vermelho, (300, 200), 10, 10)
+    # Desenhar retângulo
+    pygame.draw.rect(tela, azul, rect1)
+    pygame.draw.rect(tela, azul, rect2)
+
+
 
     #Atualizar tela
     pygame.display.update()
